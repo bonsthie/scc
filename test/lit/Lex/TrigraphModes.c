95e332eed597434d26ccd9801e93a43ee838baa5
bonsthie <barnabe.bonnet@gmail.com>
2026-04-09 18:29:32 +0200

    update Lit test to have warning in the test

--- test/lit/Lex/TrigraphModes.c
+++ test/lit/Lex/TrigraphModes.c
@@ -3,23 +3,23 @@
 // RUN: @scc_cc1 -dump-raw-tokens --std=c99 -Wno-trigraphs -Wtrigraphs %s 2>&1 | FileCheck %s --check-prefix=MIX-WARN
 // RUN: @scc_cc1 -dump-raw-tokens --std=c89 -Wtrigraphs -Wno-trigraphs %s 2>&1 | FileCheck %s --check-prefix=MIX-NO-WARN
 
-// C89-NOT: trigraph ignored [-Wtrigraphs]
+// C89-NOT: warning: trigraph ignored [-Wtrigraphs]
 // C89: pp_hash '#'
 // C89-NOT: question '?'
 
-// C99: trigraph ignored [-Wtrigraphs]
+// C99: warning: trigraph ignored [-Wtrigraphs]
 // C99: question '?'
 // C99: question '?'
 // C99: equal '='
 // C99-NOT: pp_hash '#'
 
-// MIX-WARN: trigraph ignored [-Wtrigraphs]
+// MIX-WARN: warning: trigraph ignored [-Wtrigraphs]
 // MIX-WARN: question '?'
 // MIX-WARN: question '?'
 // MIX-WARN: equal '='
 // MIX-WARN-NOT: pp_hash '#'
 
-// MIX-NO-WARN-NOT: trigraph ignored [-Wtrigraphs]
+// MIX-NO-WARN-NOT: warning: trigraph ignored [-Wtrigraphs]
 // MIX-NO-WARN: pp_hash '#'
 // MIX-NO-WARN-NOT: question '?'
 
